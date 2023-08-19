8d6025d8b5c24381e6073c543203f71fc8ac7a99
Ammar <ammar282n@hotmail.com>
2023-08-20 00:14:00 +0300

    more changes

--- school_app/views.py
+++ school_app/views.py
@@ -2,6 +2,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse("Hello Kenda")
+    return HttpResponse("Hello Kenda Thanks")
 
 # Create your views here.
